834b52c438b443e45601b53f1d15ad015c1bad7d
Laura Kibben <smartgirlpdx@gmail.com>
2016-10-31 10:55:24 -0700

    Lesson 6 complete

--- spec/entry_spec.rb
+++ spec/entry_spec.rb
@@ -33,7 +33,6 @@ RSpec.describe Entry do
           it "prints an entry as a string" do
             entry = Entry.new('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
             expected_string = "Name: Ada Lovelace\nPhone Number: 010.012.1815\nEmail: augusta.king@lovelace.com"
-        
             expect(entry.to_s).to eq(expected_string)
           end
         end
